3a3a8a418780c083d6c0793fbba2af45a33962f8
Arthur <arturkaizer@gmail.com>
2021-12-09 00:01:38 -0300

    exercicio lista resolvido

---
+++ Seção6 - Comportamento de memória, arrays, listas/exercicioListas/exercicioListas/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+namespace exercicioListas
+{
+    class Employee
+    {
+        public int id { get; private set; }
+        private string name { get; set; }
+        private double salary { get; set; }
+
+        public Employee() { }
+        public Employee(int id, string name, double salary)
+        {
+            this.id = id;
+            this.name = name;
+            this.salary = salary;
+        }
+
+        public void increaseSalary(double percentage)
+        {
+            salary += salary * (percentage / 100);
+        }
+
+        public override string ToString()
+        {
+            return id + ", " + name + ", " + salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Seção6 - Comportamento de memória, arrays, listas/exercicioListas/exercicioListas/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace exercicioListas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List < Employee > employeesList = new List<Employee>();
+            Console.Write("How many employees will be registered? ");
+            int employeesQuantity = int.Parse(Console.ReadLine());
+            for(int i = 1; i <= employeesQuantity; i++)
+            {
+                Console.WriteLine($"Emplyoee #{i}:");
+                employeesList.Add(getEmployeeInfo());
+                Console.WriteLine();
+            }
+
+            Console.Write("Enter the employee id that will have salary increase : ");
+            int searchId = int.Parse(Console.ReadLine());
+            Employee emp = employeesList.Find(x => x.id == searchId);
+            if (emp != null)
+            {
+                Console.Write("Enter the percentage: ");
+                double percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                emp.increaseSalary(percentage);
+            }
+            else
+            {
+                Console.WriteLine("This id does not exist!");
+            }
+
+            printList(employeesList);
+
+        }
+
+        static Employee getEmployeeInfo()
+        {
+            Console.Write("Id: ");
+            int id = int.Parse(Console.ReadLine());
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Salary: ");
+            double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            return new Employee(id, name, salary);
+        }
+
+        static void printList(List<Employee> employeesList)
+        {
+            foreach(Employee employee in employeesList)
+            {
+                Console.WriteLine(employee);
+            }
+        }
+        
+    }
+}
