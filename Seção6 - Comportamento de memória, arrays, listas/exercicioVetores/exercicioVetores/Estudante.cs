e74105cbe5279e3c3b5a05ebe08a1fc4a653d3eb
Arthur <arturkaizer@gmail.com>
2021-12-08 23:25:44 -0300

    exercicio vetor resolvido

---
+++ Seção6 - Comportamento de memória, arrays, listas/exercicioVetores/exercicioVetores/Estudante.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exercicioVetores
+{
+    class Estudante
+    {
+        public string nome { get; set; }
+        public string email { get; set; }
+
+        public Estudante(string nome, string email)
+        {
+            this.nome = nome;
+            this.email = email;
+        }
+
+        public override string ToString()
+        {
+            return nome+", "+email;
+        }
+    }
+}
---
+++ Seção6 - Comportamento de memória, arrays, listas/exercicioVetores/exercicioVetores/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace exercicioVetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudante[] quartos = new Estudante[10];
+            Console.Write("Quantos quartos serão alugados?");
+            int qtdeQuartos = int.Parse(Console.ReadLine());
+
+            registrarAluguel(quartos, qtdeQuartos);
+            mostrarAlugueis(quartos);
+
+        }
+        static void registrarAluguel(Estudante[] quartos, int qtdeQuartos)
+        {
+            for(int i = 0; i < qtdeQuartos; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Aluguel #{i+1}:");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                quartos[quarto] = new Estudante(nome, email);
+            }
+        }
+
+        static void mostrarAlugueis(Estudante[] quartos)
+        {
+            Console.WriteLine("Quartos ocupados:");
+            for(int i = 0; i < quartos.Length; i++)
+            {
+                if(quartos[i] != null)
+                {
+                    Console.WriteLine(i + ": " + quartos[i]);
+                }
+            }
+        }
+    }
+}
