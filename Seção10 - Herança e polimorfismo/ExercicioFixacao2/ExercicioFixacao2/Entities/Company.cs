cffa949a9433617e7e79d2464ec6d075bcff6043
Arthur <arturkaizer@gmail.com>
2021-12-18 07:21:53 -0300

    exercicio fixação 2 resolvido

---
+++ Seção10 - Herança e polimorfismo/ExercicioFixacao2/ExercicioFixacao2/Entities/Company.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioFixacao2.Entities
+{
+    class Company : TaxPayer
+    {
+       private int NumOfEmployees { get; set; }
+
+        public Company(string name, double anualIncome,int numOfEmployees) : base(name, anualIncome)
+        {
+            NumOfEmployees = numOfEmployees;
+        }
+
+        public override double Tax()
+        {
+            if(NumOfEmployees > 10)
+            {
+                return AnualIncome * 0.14;
+            }
+            else
+            {
+                return AnualIncome * 0.16;
+            }
+        }
+    }
+}
