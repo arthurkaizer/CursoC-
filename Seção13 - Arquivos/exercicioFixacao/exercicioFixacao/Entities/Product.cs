e800c5e601794e01ed76ff0a0c1635a25ae7befc
Arthur <arturkaizer@gmail.com>
2021-12-19 17:47:23 -0300

    exercicio de fixação feito

---
+++ Seção13 - Arquivos/exercicioFixacao/exercicioFixacao/Entities/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exercicioFixacao.Entities
+{
+    class Product
+    {
+        public string description { get; private set; }
+        public double price { get; private set; }
+        public int quantity { get; private set; }
+
+        public Product(string description, double price, int quantity)
+        {
+            this.description = description;
+            this.price = price;
+            this.quantity = quantity;
+        }
+
+        public double Total()
+        {
+            return price * quantity;
+        }
+
+    }
+}
