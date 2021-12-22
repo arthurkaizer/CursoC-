3d15c04a5bb03607af796fb0fde2ae35a8d35d74
Arthur <arturkaizer@gmail.com>
2021-12-22 07:21:48 -0300

    Create simple form

--- SalesWebMVC/salesWebMvc/salesWebMvc/Controllers/SellersController.cs
+++ SalesWebMVC/salesWebMvc/salesWebMvc/Controllers/SellersController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SalesWebMvc.Services;
+using SalesWebMvc.Models;
 
 namespace SalesWebMvc.Controllers
 {
@@ -20,6 +21,18 @@ namespace SalesWebMvc.Controllers
         {
             var list = _SellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _SellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
