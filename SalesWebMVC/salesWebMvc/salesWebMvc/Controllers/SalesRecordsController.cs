73a7b0b847c9258a5e5e76a93743d7c5c5c13a79
Arthur <arturkaizer@gmail.com>
2021-12-22 21:13:39 -0300

    Preparing sales search navigation views

---
+++ SalesWebMVC/salesWebMvc/salesWebMvc/Controllers/SalesRecordsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Controllers
+{
+    public class SalesRecordsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> SimpleSearch(DateTime? minDate, DateTime? maxDate)
+        {
+            return View();
+        }
+        public async Task<IActionResult> GroupingSearch(DateTime? minDate, DateTime? maxDate)
+        {
+            return View();
+        }
+    }
+}
